f12c80de671901f4121daabd35931ffb9ad07770
themida0 <63951927+themida0@users.noreply.github.com>
2021-01-31 01:19:52 +0600

    Create setting.py

---
+++ setting.py
@@ -0,0 +1,32 @@
+""" Файл настройки """
+
+""" Логин и пароль от аккаунта Gincore """
+LOGIN = ""
+PASS = ""
+
+
+""" Логин и пароль от кабинета SMS Center """
+SMS_LOGIN = ""
+SMS_PASSWORD = ""
+
+""" Страница авторизации Gincore """
+URL_AUTH= "https://***.gincore.net/auth/login"
+
+
+""" Страница печати гарантийного талона без номера заказа """
+URL_WRNT = "https://***.gincore.net/prints?act=gt-ekspress-remont&object_id="
+
+
+""" Сообщение в СМС""" 
+# Переменная [id] - номер заказа.
+SMS_TEXT = 'Ваш заказ №[id] выдан. Электронная гарантия доступна по адресу: https://site.com?id=[id]'
+
+
+""" Редирект при переходе на главную """
+REDIRECT_URL = 'https://site.com'
+
+""" Настройка телеграм бота """
+BOT_TOKEN = 'bot token'
+USER_ID = 123
+
+
